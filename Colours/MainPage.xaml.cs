cf2f4d9892d3925bc3907e862cd19d9fe3c18d70
James Hawley <jamesrichardhawley@gmail.com>
2017-11-05 09:08:12 -0500

    New pages, full navview list of items

--- Colours/MainPage.xaml.cs
+++ Colours/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Colours.Views;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -27,9 +28,62 @@ namespace Colours
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
         {
+            // set the initial SelectedItem 
+            foreach (NavigationViewItemBase item in NavView.MenuItems)
+            {
+                if (item is NavigationViewItem && item.Tag.ToString() == "home")
+                {
+                    NavView.SelectedItem = item;
+                    break;
+                }
+            }
+        }
 
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                ContentFrame.Navigate(typeof(SettingsPage));
+            }
+            else
+            {
+                switch (args.InvokedItem)
+                {
+                    case "Home":
+                        ContentFrame.Navigate(typeof(HomePage));
+                        break;
+
+                    case "Red":
+                        ContentFrame.Navigate(typeof(RedPage));
+                        break;
+                }
+            }
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                ContentFrame.Navigate(typeof(SettingsPage));
+            }
+            else
+            {
+
+                NavigationViewItem item = args.SelectedItem as NavigationViewItem;
+
+                switch (item.Tag)
+                {
+                    case "home":
+                        ContentFrame.Navigate(typeof(HomePage));
+                        break;
+
+                    case "red":
+                        ContentFrame.Navigate(typeof(RedPage));
+                        break;
+                }
+            }
         }
     }
 }
